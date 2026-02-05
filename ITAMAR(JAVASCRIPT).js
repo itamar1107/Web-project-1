461c1779e571a1c3f55a56bb7d8e88c3c17328b2
itamar1107 <kahalaniitamar@gmail.com>
2026-02-05 18:40:47 +0200

    Add initial web project files

---
+++ ITAMAR(JAVASCRIPT).js
@@ -0,0 +1,12 @@
+function moveMessi() {
+    var x = document.getElementById("messi");
+    x.classList.add("animationMessi");
+}
+function moveNeymar() {
+    var x = document.getElementById("neymar");
+    x.classList.add("animationNeymar");
+}
+function moveLogo() {
+    var x = document.getElementById("logo");
+    x.classList.add("animationLogo");
+}
